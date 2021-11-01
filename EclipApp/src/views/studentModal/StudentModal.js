118e0d270b5bfa3b30bf5d4c203d7ca1c472b4a2
Kyler Haag <kylerhaag@gmail.com>
2021-11-01 13:34:07 -0500

    updated counters

--- EclipApp/src/views/studentModal/StudentModal.js
+++ EclipApp/src/views/studentModal/StudentModal.js
@@ -17,8 +17,9 @@ import img3 from "../../assets/images/big/img4.jpg";
 
 function Counter(props) {
   const handleClick = () => props.onClickFunction(props.increment);
+  const warningColor = props.curCount < 10 ? "primary" : "danger";
   return (
-    <Button color={props.color} onClick={handleClick}>
+    <Button color={warningColor} onClick={handleClick}>
       <i className={props.icon}></i> {props.message} {props.curCount}
     </Button>
   );
@@ -35,12 +36,14 @@ const ModalComponent = props => {
     setModal(!modal);
   };
 
-  const [counter, setCounter] = useState(0);
-  const incrementCounter = () => setCounter(counter + 1);
   const [counter1, setCounter1] = useState(0);
   const incrementCounter1 = () => setCounter1(counter1 + 1);
   const [counter2, setCounter2] = useState(0);
   const incrementCounter2 = () => setCounter2(counter2 + 1);
+  const [counter3, setCounter3] = useState(0);
+  const incrementCounter3 = () => setCounter3(counter3 + 1);
+  // const [counter4, setCounter4] = useState(0);
+  // const incrementCounter4 = () => setCounter4(counter4 + 1);
 
   const externalCloseBtn = (
     <button
@@ -74,10 +77,12 @@ const ModalComponent = props => {
             </CardTitle>
 
             <CardBody className="">
-              <Button color="danger" onClick={toggle.bind(null)}>
+              <Button color="success" onClick={toggle.bind(null)}>
                 Student's Info
               </Button>
               <Modal
+                size="lg"
+                style={{ maxWidth: "700px", width: "100%" }}
                 isOpen={modal}
                 toggle={toggle.bind(null)}
                 className={props.className}
@@ -92,26 +97,17 @@ const ModalComponent = props => {
                       <CardTitle>Alexandra Sparks</CardTitle>
                       <CardSubtitle>English 1 (2nd Period)</CardSubtitle>
                       <CardText>Any Text we want here</CardText>
-                      <Row>
-                        <Col>
-                          <Counter
-                            color={"warning"}
-                            onClickFunction={incrementCounter}
-                            increment={1}
-                            message={"Hall Pass"}
-                            icon={"icon-logout"}
-                          />
-                          <Display message={counter} />
-                        </Col>
-
+                      <Row className="button-group">
+                        {/* <Display message={counter3} /> */}
                         <Counter
                           color={"primary"}
                           onClickFunction={incrementCounter1}
                           increment={1}
-                          message={"Electronics Warning"}
+                          message={"Phone Warning"}
                           icon={"mr-2 mdi mdi-cellphone-iphone"}
+                          curCount={counter1}
                         />
-                        <Display message={counter1} />
+                        {/* <Display message={counter1} /> */}
 
                         <Counter
                           color={"secondary"}
@@ -121,8 +117,24 @@ const ModalComponent = props => {
                           icon={"icon-volume-2"}
                           curCount={counter2}
                         />
+                        <Counter
+                          color={"warning"}
+                          onClickFunction={incrementCounter3}
+                          increment={1}
+                          message={"Hall Pass"}
+                          icon={"icon-logout"}
+                          curCount={counter3}
+                        />
+                        <Counter
+                          color={"danger"}
+                          //onClickFunction={incrementCounter4}
+                          increment={1}
+                          message={"Total Warnings"}
+                          icon={"icon-logout"}
+                          curCount={counter1 + counter2}
+                        />
                         <Button color="secondary" onClick={toggle.bind(null)}>
-                          Cancel
+                          Close
                         </Button>
                       </Row>
                     </CardBody>
